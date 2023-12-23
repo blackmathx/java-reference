6e6e5c42f9229d2feb4963f41ef229b4c5b246d2
jedpayt <jepayton12013@gmail.com>
2023-12-23 01:07:34 -0500

    update

--- src/main/java/b_Arrays.java
+++ src/main/java/b_Arrays.java
@@ -1,41 +1,40 @@
 
-
 import java.util.Arrays;
 import java.util.List;
 
 public class b_Arrays {
 
-
   public static void runner() {
+
     System.out.println("\n=== Arrays ==================\n");
 
     int[] first = new int[] { 1, 4, 3, 6, 9, 5, 8 };
-    int second[] = { 5, 4, 2 };
+    int[] second = {4, 3, 1, 4, 5};
     char[] third = "test".toCharArray();
-    String[] fourth = { "test", "ing", "yeah", "bird" };
+    String[] fourth = { "mock", "ing", "yeah", "bird" };
     Integer[] fifth = { 4, 3, 23, 26, 21, 29, 18, 28 };
 
-    int a = first.length; // returns length
+    int len = first.length; // returns length
     int[] clone = first.clone(); //
     int intAt2 = first[2]; // 2
     Arrays.sort(first); // [1, 2, 4, 6]
     String str = first.toString(); // returns the address
+    String str2 = Arrays.toString(first); // returns string
     List<Integer> mylist = Arrays.asList(fifth);
     System.out.println(mylist);
     
     Arrays.sort(fifth);
     Integer[] acopy = Arrays.copyOf(fifth, 4);
-    System.out.println(Arrays.asList(acopy));
+    List<Integer> intList = Arrays.asList(acopy);
     System.out.println(Arrays.toString(Arrays.copyOf(first, 2)));
 
 
     System.out.println(Arrays.toString(Arrays.copyOfRange(first, 2, 5)));
     Arrays.sort(first);
     Arrays.fill(first, 5);
-    Arrays.asList(acopy);
-    Arrays.compare(fifth, acopy);
-    Arrays.equals(first, second);
-    Arrays.binarySearch(first, 9);
+    int compare = Arrays.compare(fifth, acopy); // returns 0 if equal, <0 if first is less, >0 if first is bigger
+    boolean boolEqual = Arrays.equals(first, second);
+    int intSearch = Arrays.binarySearch(first, 9); // needs to be sorted; returns index or if not found, the negative of the index where it should go + 1;
 
 
     
