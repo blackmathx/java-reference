46d9913ba30fcbf3ca1d8b5fe152feeab78840c4
jedpayt <jepayton12013@gmail.com>
2025-01-20 13:40:51 -0500

    update

--- src/main/java/_Main.java
+++ src/main/java/_Main.java
@@ -3,20 +3,19 @@ public class _Main {
 
     public static void main( String[] args ) {
 
-        // TODO review the class examples for completeness and correctness
 
 //        a_Arrays.run();               // reviewed
 //        b_Strings.run();              // reviewed
 //        c_Numbers.run();              // reviewed
 //        d_Collections.run();          // reviewed
 //        e_DateAndTime.run();          // reviewed
+
+        // TODO review the class examples for completeness and correctness
 //        f_FileIO.run();
 //        g_LambdasAndStreams.run();
 //        h_Enums.run();
 //        i_Threads.run();
-//        z_QuickReference.run();
-
-
+//        z_QuickReference.run();       // reviewed, incomplete
 
 //        z_Sandbox.run();
 //        lambda._Runner.run();
